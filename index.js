238c32801fb7b624cc1167f8518ee73ebea01d70
Michael <oliengo.michael@gmail.com>
2024-04-02 21:50:12 +0300

    complete Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+// Test-1
+var num1 = 31;
+var num2 = 2;
 
-// Write your code here
+var multiply = num1 * num2;
+// Test-2
+var random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+// Test-3
+var num3 = 24;
+var num4 = 10;
+
+var mod = (num3 % num4);
+// Test-4
+
+var max = Math.max(3, 5, 12, 20, 4, 9);
+
